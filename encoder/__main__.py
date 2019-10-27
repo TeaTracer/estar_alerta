c7a8f0163d2b229f47500ad3d2b13882a82a58d0
Aleksandr Kiryushin <teatracer7@yandex.ru>
2019-10-27 17:01:54 +0300

    Add minio and docker-compose

--- encoder/__main__.py
+++ encoder/__main__.py
@@ -1,4 +1,5 @@
+import os
 from encoder.server import main
 
 if __name__ == '__main__':
-    main("0.0.0.0", 8080)
+    main(os.environ.get("ENCODER_HOST"), int(os.environ.get("ENCODER_PORT")))
--- encoder/server.py
+++ encoder/server.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import asyncio
 import json
@@ -6,12 +7,34 @@ from aiohttp import web
 logging.basicConfig(level=logging.INFO, format="%(asctime)s\t%(levelname)s\t%(message)s")
 logger = logging.getLogger("encoder")
 
+class NoStorageError(Exception):
+    """ no connection to storage """
+    pass
+
 async def healthcheck(request):
     logger.info("healthcheck %s", request)
-    return web.Response(body=b"OK\n")
+    status = b"OK\n"
+    return web.Response(body=status)
 
 async def on_shutdown(app):
-    logger.info("handle shutdown %s", app)
+    logger.info("on shutdown %s", app)
+
+async def on_startup(app):
+    logger.info("on startup %s", app)
+    minio_host = os.environ.get("MINIO_HOST")
+    minio_port = int(os.environ.get("MINIO_PORT"))
+    minio_access_key = os.environ.get("MINIO_ACCESS_KEY")
+    minio_secret_key = os.environ.get("MINIO_SECRET_KEY")
+    minio_init_delay = int(os.environ.get("MINIO_INIT_DELAY"))
+    minio_init_tries = int(os.environ.get("MINIO_INIT_TRIES"))
+    command = f"mc config host add minio http://{minio_host}:{minio_port} {minio_access_key} {minio_secret_key}"
+    for i in range(minio_init_tries):
+        logger.info("minio init %d %s", i + 1, command)
+        rc = await run_command(command)
+        if rc == 0:
+            return
+        await asyncio.sleep(minio_init_delay)
+    raise NoStorageError()
 
 async def task(request):
     logger.info("task %s", request)
@@ -29,20 +52,24 @@ async def task(request):
     return web.Response(body=b"DONE\n")
 
 async def run_encoder(command):
-    cmd = "ffmpeg " + command
-    logger.info(cmd)
+    await run_command("ffmpeg " + command)
+
+async def run_command(command):
+    logger.info(command)
     proc = await asyncio.create_subprocess_shell(
-        cmd,
+        command,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE)
 
     stdout, stderr = await proc.communicate()
 
-    logger.info(f'[{cmd!r} exited with {proc.returncode}]')
+    logger.info(f'[{command!r} exited with {proc.returncode}]')
     if stdout:
         logger.info(f'[stdout]\n{stdout.decode()}')
     if stderr:
         logger.info(f'[stderr]\n{stderr.decode()}')
+
+    return proc.returncode
 
 def main(host, port):
     app = web.Application()
@@ -50,6 +77,7 @@ def main(host, port):
     app.router.add_route('GET', '/healthcheck/', healthcheck)
     app.router.add_route('POST', '/task/', task)
 
+    app.on_startup.append(on_startup)
     app.on_shutdown.append(on_shutdown)
     logger.info("up at %s:%d", host, port)
     web.run_app(app, host=host, port=port, print=None)
